16bcef833793966d1ef92aab9b4a6aa5b026bbe8
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-12-19 23:41:56 +0100

    add example to readme
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,42 @@
 )]
 
 //! Async TLS streams
+//!
+//! # Examples
+//!
+//! Create an HTTP client:
+//!
+//! ```no_run
+//! # fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> { async_std::task::block_on(async {
+//! #
+//! use async_std::prelude::*;
+//! use std::net::ToSocketAddrs;
+//!
+//! // First up, resolve google.com
+//! let addr = "google.com:443".to_socket_addrs()?.next().unwrap();
+//!
+//! let socket = async_std::net::TcpStream::connect(&addr).await?;
+//!
+//! // Send off the request by first negotiating an SSL handshake, then writing
+//! // of our request, then flushing, then finally read off the response.
+//! let builder = native_tls::TlsConnector::builder();
+//! let connector = builder.build()?;
+//! let connector = async_native_tls::TlsConnector::from(connector);
+//! let mut socket = connector.connect("google.com", socket).await?;
+//! socket.write_all(b"GET / HTTP/1.0\r\n\r\n").await?;
+//!
+//! let mut data = Vec::new();
+//! socket.read_to_end(&mut data).await?;
+//!
+//! // any response code is fine
+//! assert!(data.starts_with(b"HTTP/1.0 "));
+//!
+//! let data = String::from_utf8_lossy(&data);
+//! let data = data.trim_end();
+//! assert!(data.ends_with("</html>") || data.ends_with("</HTML>"));
+//! #
+//! # Ok(()) }) }
+//! ```
 
 use std::fmt;
 use std::future::Future;
